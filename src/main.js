45a3f1e040a5dfd1d6b1894a343338f60b65f53c
Tina <tona14520@gmail.com>
2021-10-12 15:10:03 +0200

    first

--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
 import * as VueGoogleMaps from "vue2-google-maps";
-import VueFormulate from '@braid/vue-formulate'
 import SweetModal from 'sweet-modal-vue/src/plugin.js'
 
 Vue.use(SweetModal)
-Vue.use(VueFormulate)
+
 Vue.config.productionTip = false
 Vue.config.performance = true
 Vue.use(CoreuiVue)
